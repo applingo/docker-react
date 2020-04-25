5b48668341fe9ec5e38dd5263647bcc3b98116a4
applingo <applingo55@gmail.com>
2020-04-25 13:40:09 +0900

    auto-deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
+openssl aes-256-cbc -K $encrypted_dfdcfd5172af_key -iv $encrypted_dfdcfd5172af_iv -in deploy_key.enc -out ./deploy_key -d
+
 ssh centos@140.227.122.189 -i ./deploy_key -p 22 
 docker-compose up -d
